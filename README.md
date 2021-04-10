6ce1bc75a48d8118447040520f480613ac39eae1
AyushAmbastha <ambastha.ayush@gmail.com>
2021-04-10 12:57:48 -0700

    Cleanup + README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Openscap-RuleTest
+# OpenSCAP scanner rule test
+
+This repository contains a script to check whether the rule ‘sshd_set_idle_timeout’ from scap-security-guide is correctly evaluated by the openscap scanner.
+
+The script doesn't do any remidiation. It prepares the environment by installing the packages, executes the test/evaluation functions, and cleans up after itself.
+
+The rule 'sshd_set_idle_timeout’ is a part of the profile PCI-DSS v3.2.1 Control Baseline for Fedora.
--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 if [[ $UID -ne '0' ]]
 then
     echo 'Error: You need to be root user to run this test!'
-    exit ${XCCDF_RESULT_ERROR}
+    exit ${XCCDF_RESULT_FAIL}
 fi
 
 RET=${XCCDF_RESULT_PASS}
@@ -13,28 +13,16 @@ SSHD_CONFIG='/etc/ssh/sshd_config'
 function setup () {
     #If repositories are not configured
     if [ ! -d $REPO ]; then
-        RET=${XCCDF_RESULT_FAIL}
-        echo "Error: Repositories are not configured to continue to setup"
-        exit ${RET}
+        echo "FATAL ERROR: Repositories are not configured for setup to continue!"
     fi
 
-    #dnf update
+    #Download the required packages
     for package in scap-security-guide openscap openscap-scanner; do
         if ! rpm -qa | grep -qw $package; then
             dnf install $package -y
         fi
     done
     echo 'Success: Packages installed!'
-
-    #If this isn't there already - make changes
-    #if ! grep -Fxq "ClientAliveInterval 0" $SSHD_CONFIG; then
-    #    echo "ClientAliveInterval 200" >> $SSHD_CONFIG
-    #fi
-
-    #if ! grep -Fxq "ClientAliveCountMax 0" $SSHD_CONFIG; then
-    #    echo "ClientAliveCountMax 0" >> $SSHD_CONFIG
-    #fi
-
     echo "Success: Setup Complete!"
 }
 
@@ -50,7 +38,6 @@ function check_client_alive_count() {
     if ! grep -qP "^(?=[\s]*+[^#])[^#]*(ClientAliveCountMax 0)" $SSHD_CONFIG; then
         echo 'ERROR: ClientAliveCountMax needs to be set to 0 in the sshd_config file!'
         echo 'Expected Value: This is number of null packets that will be sent to an unresponsive client.'
-        #exit $XCCDF_RESULT_FAIL
     else echo 'Pass: ClientAliveCountMax configured correctly in sshd_config'
     fi
 }
@@ -63,31 +50,21 @@ function check_client_alive_interval() {
     fi
 }
 
-#Status does nothing to the overall result
-function check_sshd_status() {
-    systemctl show sshd | grep -iq "activestate=active"
-	if [ $? -ne 0 ]; then
-		echo "ERROR: SSH daemon is not running!"
-	fi
-}
-
-#gpgcheck is disabled?
-
 function openscap_report() {
+    #Evaluating the rule
     oscap xccdf eval --profile pci-dss --rule \
         xccdf_org.ssgproject.content_rule_sshd_set_idle_timeout \
         /usr/share/xml/scap/ssg/content/ssg-fedora-ds.xml
 }
 
 function clean () {
-    #sed -i '/ClientAliveCountMax 0/d' $SSHD_CONFIG
-    #sed -i '/ClientAliveInterval 200/d' $SSHD_CONFIG
-
+    #openscap-scanner and scap-security-guide are dependencies of openscap and get uninstalled together
     dnf remove openscap -y
     echo "Success: Cleanup Complete!"
+    exit $RET
 }
 
-#Calling the functions
+#Main: Calling the functions
 setup
 check_sshd_config_exists
 check_client_alive_count
